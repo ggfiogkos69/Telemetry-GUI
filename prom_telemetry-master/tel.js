0841c38e40d182d70f058b50f6011ccdc0e6dd94
ggfiogkos69 <christsourvel@gmail.com>
2023-05-30 17:04:24 +0300

    modifications

--- prom_telemetry-master/tel.js
+++ prom_telemetry-master/tel.js
@@ -55,6 +55,45 @@ let apiRequest = new XMLHttpRequest();
   }
 }
 
+function updateChart(value1, value2, value3){ // value1 = right top chart(0-100), value2 = left top chart(0-200), value3 = bottom chart (0-10000) 
+        console.log("PANIKK")
+        //here you can change the class of the signals in the middle container - ie change them from red to green or from green to red
+        var element1 = document.getElementById("IMD") 
+        if(element1.className == "live-icon red") console.log("found")
+        if(value1 > 80 && element1.className == "live-icon red") {
+          element1.classList.remove("red");
+          element1.classList.add("green");
+          console.log("not here")
+    
+         }
+
+          chartInstance.data.labels.push(new Date());
+          chartInstance.data.datasets.forEach((dataset) => {
+              dataset.data.push(value2);
+          });
+          chartInstance2.data.labels.push(new Date());
+          chartInstance2.data.datasets.forEach((dataset) => {
+              dataset.data.push(value3);
+          });
+      
+              chartInstance.data.labels.splice(0, 1);
+        chartInstance.data.datasets[0].data.splice(0, 1);	
+                  
+     
+     
+          chartInstance2.data.labels.splice(0, 1);
+              chartInstance2.data.datasets[0].data.splice(0, 1);
+      
+      //chartInstance3.data.datasets[0].data = [value3, 100-value3];
+      var txt = value1.toString();
+      //chartInstance3.options.elements.center.text = txt;
+          //chartInstance3.update();
+          chartInstance.update(0);
+          chartInstance2.update(0);
+    
+      };   
+     
+
 const results = []
 function isjson(str) {
   try {
